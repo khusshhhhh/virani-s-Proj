60cca7af06a7e435d4c6721a2509df82d59cc4ad
khusshhhhh <workforkhush8@gmail.com>
2024-12-14 19:28:32 +1030

    bcrypt issue solving

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { Pool } = require("pg");
 const path = require("path");
 
